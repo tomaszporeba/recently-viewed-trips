577d370b9fbf1f9e8ce74940badb0ed8d4606620
tporeba <tomasz.poreba@ringieraxelspringer.pl>
2023-01-04 12:13:46 +0100

    update fav

--- src/containers/Head.tsx
+++ src/containers/Head.tsx
@@ -7,7 +7,7 @@ function HeadContainer() {
             <title>Travcorp Poland Interview Task</title>
             <meta name="description" content="Interview app for Travcorp Poland"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <link rel="icon" href="/favicon.ico"/>
+            <link href="https://uploads-ssl.webflow.com/633c1cbe12c2cf0aab75aae5/6358e0ce2749b87bc2ff98cd_Favicon%20-%20small.png" rel="shortcut icon" type="image/x-icon"/>
             <meta property="og:site_name" content="recently-viewed-trips"/>
             <meta property="og:type" content="website"/>
         </Head>
